12cc3377df48125c40792e64b581c2c823914f39
lucas.gaspar <lucas.gaspar@cwi.com.br>
2017-04-23 16:35:30 -0300

    feat(Saint): Classe de Batalha Atualizda para só parar a Batalha quando um Saint morrer. Lista 5 concluída

--- cavaleiros/Batalha.java
+++ cavaleiros/Batalha.java
@@ -7,16 +7,59 @@ public class Batalha {
         this.saint2 = saint2;
     }
 
+    // public void iniciar() {
+    // int valor1 = this.saint1.getArmadura().getCategoria().getValor();
+    // int valor2 = this.saint2.getArmadura().getCategoria().getValor();
+    // final double dano = 10;
+
+    // if (valor1 >= valor2) {
+    // this.saint2.perderVida(dano);
+    // } else {
+    // this.saint1.perderVida(dano);
+    // }
+
+    // }
+
     public void iniciar() {
         int valor1 = this.saint1.getArmadura().getCategoria().getValor();
         int valor2 = this.saint2.getArmadura().getCategoria().getValor();
-        final double dano = 10;
+        Saint proximoAGolpear;
+        Saint golpeado;
 
         if (valor1 >= valor2) {
-            this.saint2.perderVida(dano);
+            Golpear g1 = new Golpear(this.saint1, this.saint2);
+            g1.executar();
+            proximoAGolpear = this.saint2;
+            if(this.saint2.getStatus().equals(Status.MORTO)){
+                return;
+            }
         } else {
-            this.saint1.perderVida(dano);
+            Golpear g1 = new Golpear(this.saint2, this.saint1);
+            g1.executar();
+            proximoAGolpear = this.saint1;
+            if(this.saint1.getStatus().equals(Status.MORTO)){
+                return;
+            }
         }
 
+        while(this.saint1.getStatus().equals(Status.VIVO) && this.saint2.getStatus().equals(Status.VIVO)){
+
+            if(proximoAGolpear.equals(this.saint1)){
+                golpeado = this.saint2;
+            } else {
+                golpeado = this.saint1;
+            }
+
+            Golpear g1 = new Golpear(proximoAGolpear,golpeado);
+            g1.executar();
+
+            if(proximoAGolpear.equals(this.saint1)){
+                proximoAGolpear = this.saint2;
+            }
+            else{
+                proximoAGolpear = this.saint1;
+            }
+
+        }
     }
 }
--- cavaleiros/BatalhaTest.java
+++ cavaleiros/BatalhaTest.java
@@ -10,11 +10,13 @@ public class BatalhaTest {
         Saint shaina = new SilverSaint("Shaina", "Serpente");
         Saint hyoga = new BronzeSaint("Hyoga", "Cisne");
         Batalha batalha = new Batalha(shaina, hyoga);
+        shaina.getConstelacao().adicionarGolpe(new Golpe("Meteoro de Pégasus",10));
+        hyoga.getConstelacao().adicionarGolpe(new Golpe("Meteoro de Pégasus",10));
         // Act
         batalha.iniciar();
         // Assert
-        assertEquals(100, shaina.getVida(), 0.01);
-        assertEquals(90, hyoga.getVida(), 0.01);
+        assertEquals(10, shaina.getVida(), 0.01);
+        assertEquals(0, hyoga.getVida(), 0.01);
     }
     
     @Test
@@ -23,11 +25,13 @@ public class BatalhaTest {
         Saint aldebaran = new GoldSaint("Aldebaran", "Touro");
         Saint mascaraMorte = new GoldSaint("Máscara da Morte", "Câncer");
         Batalha batalha = new Batalha(aldebaran, mascaraMorte);
+        aldebaran.getConstelacao().adicionarGolpe(new Golpe("Meteoro de Pégasus",10));
+        mascaraMorte.getConstelacao().adicionarGolpe(new Golpe("Meteoro de Pégasus",10));
         // Act
         batalha.iniciar();
         // Assert
-        assertEquals(100, aldebaran.getVida(), 0.01);
-        assertEquals(90, mascaraMorte.getVida(), 0.01);
+        assertEquals(10, aldebaran.getVida(), 0.01);
+        assertEquals(0, mascaraMorte.getVida(), 0.01);
     }
     
     @Test
@@ -36,10 +40,48 @@ public class BatalhaTest {
         Saint ikki = new BronzeSaint("Ikki", "Fênix");
         Saint mascaraMorte = new GoldSaint("Máscara da Morte", "Câncer");
         Batalha batalha = new Batalha(ikki, mascaraMorte);
+        ikki.getConstelacao().adicionarGolpe(new Golpe("Meteoro de Pégasus",10));
+        mascaraMorte.getConstelacao().adicionarGolpe(new Golpe("Meteoro de Pégasus",10));
+        // Act
         // Act
         batalha.iniciar();
         // Assert
-        assertEquals(90, ikki.getVida(), 0.01);
-        assertEquals(100, mascaraMorte.getVida(), 0.01);
+        assertEquals(0, ikki.getVida(), 0.01);
+        assertEquals(10, mascaraMorte.getVida(), 0.01);
     }
+    
+     @Test
+    public void saintComArmaduraVestidaDaMaisDano() throws Exception {
+        // Arrange
+        Saint ikki = new BronzeSaint("Ikki", "Fênix");
+        Saint mascaraMorte = new GoldSaint("Máscara da Morte", "Câncer");
+        mascaraMorte.vestirArmadura();
+        Batalha batalha = new Batalha(ikki, mascaraMorte);
+        ikki.getConstelacao().adicionarGolpe(new Golpe("Meteoro de Pégasus",10));
+        mascaraMorte.getConstelacao().adicionarGolpe(new Golpe("Meteoro de Pégasus",10));
+        // Act
+        // Act
+        batalha.iniciar();
+        // Assert
+        assertEquals(0, ikki.getVida(), 0.01);
+        assertEquals(80, mascaraMorte.getVida(), 0.01);
+    }
+    
+    @Test
+    public void saintPrataDaMaisDano() throws Exception {
+        // Arrange
+        Saint ikki = new BronzeSaint("Ikki", "Fênix");
+        Saint mascaraMorte = new SilverSaint("Máscara da Morte", "Lagartixa");
+        mascaraMorte.vestirArmadura();
+        Batalha batalha = new Batalha(ikki, mascaraMorte);
+        ikki.getConstelacao().adicionarGolpe(new Golpe("Meteoro de Pégasus",10));
+        mascaraMorte.getConstelacao().adicionarGolpe(new Golpe("Meteoro de Pégasus",10));
+        // Act
+        // Act
+        batalha.iniciar();
+        // Assert
+        assertEquals(0, ikki.getVida(), 0.01);
+        assertEquals(70, mascaraMorte.getVida(), 0.01);
+    }
+    
 }
