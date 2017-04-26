3aadfdeea6e9d0bf205a81f914efb88189721fba
lucas.gaspar <lucas.gaspar@cwi.com.br>
2017-04-26 17:31:44 -0300

    feat(Saint): Exercicios extras começando

---
+++ cavaleiros/AtaqueDuplo.java
@@ -0,0 +1,13 @@
+public class AtaqueDuplo implements Movimento{
+    
+    private Saint golpeador, golpeado;
+    
+    public AtaqueDuplo(Saint golpeador, Saint golpeado) {
+        this.golpeador = golpeador;
+        this.golpeado = golpeado;
+    }
+    
+    public void executar(){
+        
+    }
+}
