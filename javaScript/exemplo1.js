17e215063d7d4c83cba5d4fbbebcd30fcbec9ad8
lucas.gaspar <lucas.gaspar@cwi.com.br>
2017-05-11 17:36:08 -0300

    feat(javascript): aula 1 do modulo

---
+++ javaScript/exemplo1.js
@@ -0,0 +1,11 @@
+console.log("carregou");
+// var pi = 3.14;
+if (typeof pi !== "undefined") {
+    console.log(pi);
+}
+
+function somar(a, b) {
+  return a+b;
+}
+
+console.log("soma igual a ", somar(1,3));
