3cd1c4839124bd630f3cae8876afc6fb521c26af
lucas.gaspar <lucas.gaspar@cwi.com.br>
2017-05-18 16:44:52 -0300

    feat(angular): alteraçoes exercicio aula - ainda n finalizado

--- modulo 5 - Angular/aula 2/controller.js
+++ modulo 5 - Angular/aula 2/controller.js
@@ -5,6 +5,9 @@ modulo.controller("dateController", function($scope, $filter){
     $scope.formatarData = formatarData;
 
     function formatarData(data) {
+        let dataformatada = data.split("/");
+        let dataConvertida = new Date(dataformatada[2],dataformatada[1]-1, dataformatada[0]);
+        console.log(dataArray);
         data = new Date(data);    
         $scope.dataformatada = $filter('date')(data, 'shortDate');
     }
