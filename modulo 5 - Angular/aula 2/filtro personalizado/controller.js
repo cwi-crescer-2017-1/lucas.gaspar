2d61c3a2bf4fa80ccb9a800c8d12ba85f5e12b40
lucas.gaspar <lucas.gaspar@cwi.com.br>
2017-05-19 01:06:52 -0300

    feat(angular): tema andamento

--- modulo 5 - Angular/aula 2/filtro personalizado/controller.js
+++ modulo 5 - Angular/aula 2/filtro personalizado/controller.js
@@ -1,13 +1,27 @@
 modulo = angular.module('modulo01', []);
 
-modulo.filter('mascada', function (){
-    return function(nome){
-        return nome.replace(/(nunes)/i, `$${nome}$`);
+modulo.filter('mascada', function () {
+    return function (nome) {
+        return nome.replace(/(nunes)/i, "$ $1 $");
     }
 });
 
 modulo.controller("filtroController", function ($scope) {
     let instrutores = [{
+        nome: 'Pedro (PHP)',
+        aula: [{
+            numero: 3,
+            nome: 'HTML e CSS'
+        }]
+    },
+    {
+        nome: 'Zanatta',
+        aula: [{
+            numero: 5,
+            nome: 'AngularJS'
+        }]
+    },
+    {
         nome: 'Bernardo',
         aula: [{
             numero: 1,
@@ -25,20 +39,7 @@ modulo.controller("filtroController", function ($scope) {
             numero: 2,
             nome: 'Banco de Dados I'
         }]
-    },
-    {
-        nome: 'Pedro (PHP)',
-        aula: [{
-            numero: 3,
-            nome: 'HTML e CSS'
-        }]
-    },
-    {
-        nome: 'Zanatta',
-        aula: [{
-            numero: 5,
-            nome: 'AngularJS'
-        }]
+
     }
     ];
     $scope.instrutores = instrutores;
