f1f971e3725cb3967a19fe19d91c4c3d5c684947
lucas.gaspar <lucas.gaspar@cwi.com.br>
2017-05-24 17:34:15 -0300

    feat(.net): primeira aula implementação

---
+++ modulo 6 - Web API dotNet/Aula1/Demo1/Calculo.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Demo1
+{
+    public class Calculo
+    {
+        public double Altura { get; set; }
+        public double Peso { get; set; }
+
+        public Calculo(double altura, double peso)
+        {
+            Altura = altura;
+            Peso = peso;
+        }
+
+        public Double calcularImc()
+        {
+            return Peso / (Altura * Altura);
+        }
+    }
+}
---
+++ modulo 6 - Web API dotNet/Aula1/Demo1/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Demo1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*Console.WriteLine("Informe o peso");
+            var entradaPeso = Console.ReadLine();
+
+            var pesoTeste = 0D;
+
+            if (!double.TryParse(entradaPeso, out pesoTeste))
+            {
+                Console.WriteLine("Entrada de peso inválida");          
+            }
+
+            Console.WriteLine("Informe a altura");
+            var entradaAltura = Console.ReadLine();
+
+            var peso = double.Parse(entradaPeso);
+            var altura = double.Parse(entradaAltura);
+
+            var calculoImc = new Calculo(altura, peso);
+            var imc = calculoImc.calcularImc();
+
+            Console.WriteLine("Seu imc é " +imc);
+            Console.ReadKey();*/
+
+            List<int> array = new List<int>();
+            Boolean continua = true;
+
+            while (continua == true)
+            {
+                Console.WriteLine("Digite Um valor");
+                var entrada = Console.ReadLine();
+
+                if (entrada == "exit")
+                {
+                    break;
+                }
+
+                array.Add(int.Parse(entrada));
+
+            }
+
+            foreach (var a in array)
+            {
+                Console.WriteLine(a);
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
