24ca040ca01cc8c7be8592f8e75d93dd8e48bd9a
lucas.gaspar <lucas.gaspar@cwi.com.br>
2017-06-01 10:37:31 -0300

    feat(.net): Exercicios prontos

--- modulo 6 - Web API dotNet/Aula5/EditoraCrescer/EditoraCrescer.Infraestrutura/Repositorios/RevisorRepositorio.cs
+++ modulo 6 - Web API dotNet/Aula5/EditoraCrescer/EditoraCrescer.Infraestrutura/Repositorios/RevisorRepositorio.cs
@@ -1,13 +1,14 @@
 ﻿using EditoraCrescer.Infraestrutura.Entidades;
 using System;
 using System.Collections.Generic;
+using System.Data.Entity;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace EditoraCrescer.Infraestrutura.Repositorios
 {
-    public class RevisorRepositorio
+    public class RevisorRepositorio : IDisposable
     {
         private Contexto contexto = new Contexto();
 
@@ -22,10 +23,30 @@ namespace EditoraCrescer.Infraestrutura.Repositorios
             contexto.SaveChanges();
         }
 
-        public void Deletar(int id)
+        public void Deletar(Revisor revisor)
         {
-            Revisor revisorASerDeletado = contexto.Revisores.FirstOrDefault(r => r.Id == id);
-            contexto.Revisores.Remove(revisorASerDeletado);
+            contexto.Revisores.Remove(revisor);
+            contexto.SaveChanges();
+        }
+
+        public Revisor ObterPorId(int id)
+        {
+            return contexto.Revisores.FirstOrDefault(r => r.Id == id);
+        }
+
+        public void Dispose()
+        {
+            contexto.Dispose();
+        }
+
+        public bool VerificaSeORevisorExiste(int id)
+        {
+            return contexto.Revisores.Count(r => r.Id == id) > 0;
+        }
+
+        public void Alterar(Revisor revisor)
+        {
+            contexto.Entry(revisor).State = EntityState.Modified;
             contexto.SaveChanges();
         }
     }
--- modulo 6 - Web API dotNet/Aula5/EditoraCrescer/EditoraCrescer.api/Controllers/RevisoresController.cs
+++ modulo 6 - Web API dotNet/Aula5/EditoraCrescer/EditoraCrescer.api/Controllers/RevisoresController.cs
@@ -9,17 +9,29 @@ using System.Web.Http;
 
 namespace EditoraCrescer.api.Controllers
 {
+    [RoutePrefix("api/Revisores")]
     public class RevisoresController : ApiController
     {
         private RevisorRepositorio repositorio = new RevisorRepositorio();
 
-        public IHttpActionResult Get()
+        [HttpGet]
+        public IHttpActionResult ObterTodos()
         {
             var revisores = repositorio.Obter();
 
-            return Ok();
+            return Ok(revisores);
         }
 
+        [HttpGet]
+        [Route("{id:int}")]
+        public IHttpActionResult ObterPorId(int id)
+        {
+            var revisores = repositorio.ObterPorId(id);
+
+            return Ok(revisores);
+        }
+
+        [HttpPost]
         public IHttpActionResult Post(Revisor revisor)
         {
             repositorio.Incluir(revisor);
@@ -27,11 +39,41 @@ namespace EditoraCrescer.api.Controllers
             return Ok();
         }
 
-        public IHttpActionResult delete(int id)
+        [Route("{id:int}")]
+        [HttpPut]
+        public HttpResponseMessage Put(int id, Revisor revisor)
         {
-            repositorio.Deletar(id);
+            if (id != revisor.Id)
+                return Request.CreateResponse(HttpStatusCode.BadRequest,
+                    new { mensagens = new string[] { "Ids não conferem" } });
 
-            return Ok();
+            if (!repositorio.VerificaSeORevisorExiste(id))
+                return Request.CreateResponse(HttpStatusCode.NotFound,
+                    new { mensagens = new string[] { "Revisor não encontrado" } });
+
+            repositorio.Alterar(revisor);
+
+            return Request.CreateResponse(HttpStatusCode.OK);
+        }
+
+        [Route("{id:int}")]
+        [HttpDelete]
+        public HttpResponseMessage delete(int id)
+        {
+            var revisor = repositorio.ObterPorId(id);
+            if (revisor == null)
+                return Request.CreateResponse(HttpStatusCode.NotFound,
+                    new { mensagens = new string[] { "Autor não encontrado" } });
+
+            repositorio.Deletar(revisor);
+
+            return Request.CreateResponse(HttpStatusCode.OK);
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            repositorio.Dispose();
+            base.Dispose(disposing);
         }
     }
 }
