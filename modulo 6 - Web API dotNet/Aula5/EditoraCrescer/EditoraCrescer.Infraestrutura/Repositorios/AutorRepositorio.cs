63c6f30006f27262668be3949e2170fa1a277fc1
lucas.gaspar <lucas.gaspar@cwi.com.br>
2017-06-01 09:41:43 -0300

    feat(.net): Sessão de autores, implementado crud

--- modulo 6 - Web API dotNet/Aula5/EditoraCrescer/EditoraCrescer.Infraestrutura/Repositorios/AutorRepositorio.cs
+++ modulo 6 - Web API dotNet/Aula5/EditoraCrescer/EditoraCrescer.Infraestrutura/Repositorios/AutorRepositorio.cs
@@ -1,13 +1,14 @@
 ﻿using EditoraCrescer.Infraestrutura.Entidades;
 using System;
 using System.Collections.Generic;
+using System.Data.Entity;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace EditoraCrescer.Infraestrutura.Repositorios
 {
-    public class AutorRepositorio
+    public class AutorRepositorio : IDisposable
     {
         private Contexto contexto = new Contexto();
 
@@ -16,18 +17,43 @@ namespace EditoraCrescer.Infraestrutura.Repositorios
             return contexto.Autores.ToList();
         }
 
+        public Autor ObterPorId(int id)
+        {
+            return contexto.Autores.FirstOrDefault(a => a.Id == id);
+        }
+
+        public List<Livro> ObterLivrosAutor(int id)
+        {
+            var autor = contexto.Autores.FirstOrDefault(a => a.Id == id);
+            return contexto.Livros.Where(l => l.Autor.Id == autor.Id).ToList();
+        }
+
         public void Incluir(Autor autor)
         {
             contexto.Autores.Add(autor);
             contexto.SaveChanges();
         }
 
-        public void Deletar(int id)
+        public void Deletar(Autor autor)
         {
-            Autor autorASerDeletado = contexto.Autores.FirstOrDefault(a => a.Id == id);
-            contexto.Autores.Remove(autorASerDeletado);
+            contexto.Autores.Remove(autor);
             contexto.SaveChanges();
         }
 
+        public bool VerificaSeOAutorExiste(int id)
+        {
+            return contexto.Autores.Count(a => a.Id== id) > 0;
+        }
+
+        public void Alterar(Autor autor)
+        {
+            contexto.Entry(autor).State = EntityState.Modified;
+            contexto.SaveChanges();
+        }
+
+        public void Dispose()
+        {
+            contexto.Dispose();
+        }
     }
 }
--- modulo 6 - Web API dotNet/Aula5/EditoraCrescer/EditoraCrescer.api/Controllers/AutoresController.cs
+++ modulo 6 - Web API dotNet/Aula5/EditoraCrescer/EditoraCrescer.api/Controllers/AutoresController.cs
@@ -9,17 +9,38 @@ using System.Web.Http;
 
 namespace EditoraCrescer.api.Controllers
 {
+    [RoutePrefix("api/Autores")]
     public class AutoresController : ApiController
     {
         private AutorRepositorio repositorio = new AutorRepositorio();
 
-        public IHttpActionResult Get()
+        [HttpGet]
+        public IHttpActionResult ObterTodos()
         {
             var autores = repositorio.Obter();
 
-            return Ok();
+            return Ok(autores);
         }
 
+        [HttpGet]
+        [Route("{id:int}")]
+        public IHttpActionResult ObterPorId(int id)
+        {
+            var autores = repositorio.ObterPorId(id);
+
+            return Ok(autores);
+        }
+
+        [HttpGet]
+        [Route("{id:int}/Livros")]
+        public IHttpActionResult ObterLivrosAutor(int id)
+        {
+            var livros = repositorio.ObterLivrosAutor(id);
+
+            return Ok(livros);
+        }
+
+        [HttpPost]
         public IHttpActionResult Post(Autor autor)
         {
             repositorio.Incluir(autor);
@@ -27,11 +48,41 @@ namespace EditoraCrescer.api.Controllers
             return Ok();
         }
 
-        public IHttpActionResult delete(int id)
+        [Route("{id:int}")]
+        [HttpPut]
+        public HttpResponseMessage Put(int id, Autor autor)
         {
-            repositorio.Deletar(id);
+            if (id != autor.Id)
+                return Request.CreateResponse(HttpStatusCode.BadRequest,
+                    new { mensagens = new string[] { "Ids não conferem" } });
 
-            return Ok();
+            if (!repositorio.VerificaSeOAutorExiste(id))
+                return Request.CreateResponse(HttpStatusCode.NotFound,
+                    new { mensagens = new string[] { "Livro não encontrado" } });
+
+            repositorio.Alterar(autor);
+
+            return Request.CreateResponse(HttpStatusCode.OK);
+        }
+
+        [Route("{id:int}")]
+        [HttpDelete]
+        public HttpResponseMessage delete(int id)
+        {
+            var autor = repositorio.ObterPorId(id);
+            if (autor == null)
+                return Request.CreateResponse(HttpStatusCode.NotFound,
+                    new { mensagens = new string[] { "Autor não encontrado" } });
+
+            repositorio.Deletar(autor);
+
+            return Request.CreateResponse(HttpStatusCode.OK);
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            repositorio.Dispose();
+            base.Dispose(disposing);
         }
 
     }
