9986ab2ee7de2d50899a743d2bbc92ee15fa4f83
lucas.gaspar <lucas.gaspar@cwi.com.br>
2017-06-02 17:37:14 -0300

    feat(EditoraCrescer):Exemplo de aula sobre como implementar autenticação

---
+++ modulo 6 - Web API dotNet/Aula5/EditoraCrescer/EditoraCrescer.api/Controllers/MeuAtributoAutorizacaoController.cs
@@ -0,0 +1,19 @@
+﻿using EditoraCrescer.api.App_Start;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace EditoraCrescer.api.Controllers
+{
+    [MeuAtributoAutorizacao(Roles ="Administrador")]
+    public class MeuAtributoAutorizacaoController : ApiController
+    {
+        public HttpResponseMessage Get()
+        {
+            return Request.CreateResponse(System.Net.HttpStatusCode.OK);
+        }
+    }
+}
