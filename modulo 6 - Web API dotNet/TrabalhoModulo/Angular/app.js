af1a0d9cb90ada110d5519e5d387287fb15cb69b
lucas.gaspar <lucas.gaspar@cwi.com.br>
2017-06-05 17:30:33 -0300

    feat(.net Trabalho): Esqueleto estrutura em angular criado

---
+++ modulo 6 - Web API dotNet/TrabalhoModulo/Angular/app.js
@@ -0,0 +1,26 @@
+var modulo = angular.module('locadora', ['ngRoute']);
+
+modulo.config(function($routeProvider) {
+    $routeProvider
+    // .when('/locacao', {
+    //   controller: 'locacaoController',
+    //   templateUrl: 'locacao/locacao.html'
+    // })
+    // .when('/devolucao', {
+    //   controller: 'devolucaoController',
+    //   templateUrl: 'devolucao/devolucao.html'
+    // })
+    // .when('/relatorio', {
+    //   controller: 'relatorioController',
+    //   templateUrl: 'relatorio/relatorio.html'
+    // })
+    .when('/cliente', {
+      controller: 'clienteController',
+      templateUrl: 'cliente/cliente.html'
+    })
+    // .when('/login', {
+    //   controller: 'loginController',
+    //   templateUrl: 'login/login.html'
+    // })
+    .otherwise({redirectTo: '/locacao'});
+});
---
+++ modulo 6 - Web API dotNet/TrabalhoModulo/Angular/cliente/clienteController.js
@@ -0,0 +1,3 @@
+modulo.controller("clienteController", function($scope){
+    
+}); 
